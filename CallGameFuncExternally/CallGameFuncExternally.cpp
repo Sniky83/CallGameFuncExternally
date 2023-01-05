f06a34f6656da0f4b3c049dc0d404ebfe8399f33
Sniky <sniky83@hotmail.fr>
2023-01-05 02:09:17 +0100

    Refactoring code Fix bugs Optimization Structuring with Regions Add addresses manipulation methods

--- CallGameFuncExternally/CallGameFuncExternally.cpp
+++ CallGameFuncExternally/CallGameFuncExternally.cpp
@@ -4,18 +4,29 @@
 #include <vector>
 #include <sstream>
 
-// 1 byte for the call instruction and 4 bytes for the addr
-CONST int SIZEOF_RELATIVE_ADDR_CALL = 5;
+#pragma region Consts
+CONST int SIZEOF_RELATIVE_ADDR_CALL = 4;
+CONST int SIZEOF_RELATIVE_ADDR_CALL_WITH_INSTRUCTION = 5;
 CONST int SIZEOF_END_SHELLCODE = 4;
+#pragma endregion
 
-#pragma region STATIC
+#pragma region Enums
+enum INSTRUCTION {
+	CALL = 0xE8,
+	PUSH = 0x68
+};
+#pragma endregion
+
+#pragma region End APP
 void PrintEndOfProgram()
 {
 	std::cout << "Press ENTER to close APP...\n";
 	int res = getchar();
 	exit(-1);
 }
+#pragma endregion
 
+#pragma region Find Process
 DWORD GetProcessId(const char* processName)
 {
 	DWORD pid = 0;
@@ -54,16 +65,30 @@ HANDLE OpenProc(const char* processName)
 
 	return hProcess;
 }
+#pragma endregion
 
-void CloseProperly(HANDLE hProcess, HANDLE hThread, LPVOID pRemoteBuffer, unsigned char* shellcode)
+#pragma region Memory
+unsigned char* CreateShellcode(unsigned char* shellcodeHeadersArray, unsigned char* relativeAddressArray, const int sizeofShellcode, const int sizeofFuncHeaders, int nbArgsPushed = 1, bool isRet = true)
 {
-	// Wait for the remote thread to finish execution
-	WaitForSingleObject(hThread, INFINITE);
-	// Free the allocated memory and close handles
-	VirtualFreeEx(hProcess, pRemoteBuffer, 0, MEM_RELEASE);
-	CloseHandle(hThread);
-	CloseHandle(hProcess);
-	free(shellcode);
+	int clearStackArgs = (4 * nbArgsPushed);
+	int retVal = (isRet == true ? 0xC3 : 0x90);
+
+	unsigned char endShellcode[] = {
+		0x83, 0xC4, clearStackArgs, // add esp, (4 x nbArgs)
+		retVal // ret or nop
+	};
+
+	// Create a new dynamic character array to hold the shellcode
+	unsigned char* shellcode = new unsigned char[sizeofShellcode];
+
+	// Copy the elements of startShellcode to the beginning of shellcode
+	memcpy(shellcode, shellcodeHeadersArray, sizeofFuncHeaders);
+	// Copy the elements of relativeAddressArr to the end of shellcode
+	memcpy(shellcode + sizeofFuncHeaders, relativeAddressArray, SIZEOF_RELATIVE_ADDR_CALL_WITH_INSTRUCTION);
+	// Copy the elements of closeShellcode to the end of shellcode
+	memcpy(shellcode + SIZEOF_RELATIVE_ADDR_CALL_WITH_INSTRUCTION + sizeofFuncHeaders, endShellcode, sizeof(endShellcode));
+
+	return shellcode;
 }
 
 LPVOID VirtualAllocation(HANDLE hProcess, const int sizeofShellcode)
@@ -100,85 +125,82 @@ HANDLE CreateRemThread(HANDLE hProcess, LPVOID pRemoteBuffer)
 
 	return hThread;
 }
+
+void CloseProperly(HANDLE hProcess, HANDLE hThread, LPVOID pRemoteBuffer, unsigned char* finalAddrBytes = NULL, unsigned char* shellcode = NULL)
+{
+	// Wait for the remote thread to finish execution
+	WaitForSingleObject(hThread, INFINITE);
+	// Free the allocated memory and close handles
+	VirtualFreeEx(hProcess, pRemoteBuffer, 0, MEM_RELEASE);
+	CloseHandle(hThread);
+	CloseHandle(hProcess);
+	delete[] finalAddrBytes;
+	delete[] shellcode;
+}
 #pragma endregion
 
-unsigned char* GetRelativeAddr(LPVOID pRemoteBuffer, DWORD callAddress, const int sizeofFuncHeaders)
+#pragma region Adresses Manipulation
+DWORD GetAddrLittleIndian(DWORD addr) {
+	unsigned char* char_array = (unsigned char*)&addr;
+	std::reverse(char_array, char_array + sizeof(DWORD));
+
+	return addr;
+}
+
+DWORD GetRelativeAddr(LPVOID pRemoteBuffer, DWORD callAddress, const int sizeofFuncHeaders)
 {
 	DWORD targetFunctionAddress = (DWORD)pRemoteBuffer;
 	DWORD relativeAddress = (callAddress - targetFunctionAddress - sizeofFuncHeaders - 5);
 
-	std::stringstream stream;
-	stream << std::hex << relativeAddress;
-	std::string hexString(stream.str());
-
-	std::vector<unsigned char> hexBytes;
-
-	hexBytes.push_back(static_cast<unsigned char>(0xE8));
-
-	for (int i = 0; i < hexString.size(); i += 2) {
-		unsigned long byte = std::stoul(hexString.substr(i, 2), nullptr, 16);
-		hexBytes.push_back(static_cast<unsigned char>(byte));
-	}
-
-	// start at index one to place E8 (call) instruction first
-	std::reverse(hexBytes.begin() + 1, hexBytes.end());
-
-	// 4 bytes for Calling addr
-	unsigned char* relativeAddressArray = new unsigned char[SIZEOF_RELATIVE_ADDR_CALL];
-	std::copy(hexBytes.begin(), hexBytes.end(), relativeAddressArray);
+	DWORD relativeAddressArray = GetAddrLittleIndian(relativeAddress);
 
 	return relativeAddressArray;
 }
 
-unsigned char* CreateShellcode(unsigned char* shellcodeHeadersArray, unsigned char* relativeAddressArray, const int sizeofShellcode, const int sizeofFuncHeaders, int nbArgsPushed = 1, bool isRet = true)
+unsigned char* GetFinalAddrWithInstruction(DWORD addr, INSTRUCTION instruction, bool isReverse = true)
 {
-	int clearStackArgs = (4 * nbArgsPushed);
-	int retVal = (isRet == true ? 0xC3 : 0x90);
+	unsigned char* addrWithFirstByteArray = new unsigned char[SIZEOF_RELATIVE_ADDR_CALL_WITH_INSTRUCTION];
 
-	unsigned char endShellcode[] = {
-		0x83, 0xC4, clearStackArgs, // add esp, (4 x nbArgs)
-		retVal // ret or nop
-	};
+	addrWithFirstByteArray[0] = instruction;
+	std::memcpy(addrWithFirstByteArray + 1, &addr, sizeof addr);
 
-	// Create a new dynamic character array to hold the shellcode
-	unsigned char* shellcode = new unsigned char[sizeofShellcode];
+	if (isReverse)
+	{
+		std::reverse(addrWithFirstByteArray + 1, addrWithFirstByteArray + SIZEOF_RELATIVE_ADDR_CALL_WITH_INSTRUCTION);
+	}
 
-	// Copy the elements of startShellcode to the beginning of shellcode
-	memcpy(shellcode, shellcodeHeadersArray, sizeofFuncHeaders);
-	// Copy the elements of relativeAddressArr to the end of shellcode
-	memcpy(shellcode + sizeofFuncHeaders, relativeAddressArray, SIZEOF_RELATIVE_ADDR_CALL);
-	// Copy the elements of closeShellcode to the end of shellcode
-	memcpy(shellcode + SIZEOF_RELATIVE_ADDR_CALL + sizeofFuncHeaders, endShellcode, sizeof(endShellcode));
-
-	return shellcode;
+	return addrWithFirstByteArray;
 }
+#pragma endregion
 
+#pragma region Create Calling Game Funcs
 void CallGameFunc(HANDLE hProcess, unsigned char* funcHeaders, DWORD callAddress, const int sizeofFuncHeaders, const int nbArgsPushed = 1)
 {
-	const int sizeofShellcode = sizeofFuncHeaders + SIZEOF_RELATIVE_ADDR_CALL + SIZEOF_END_SHELLCODE;
+	const int sizeofShellcode = sizeofFuncHeaders + SIZEOF_RELATIVE_ADDR_CALL_WITH_INSTRUCTION + SIZEOF_END_SHELLCODE;
 
 	LPVOID pRemoteBuffer = VirtualAllocation(hProcess, sizeofShellcode);
 
-	unsigned char* relativeAddrBytes = GetRelativeAddr(pRemoteBuffer, callAddress, sizeofFuncHeaders);
+	DWORD relativeAddrBytes = GetRelativeAddr(pRemoteBuffer, callAddress, sizeofFuncHeaders);
+	unsigned char* finalAddrBytes = GetFinalAddrWithInstruction(relativeAddrBytes, INSTRUCTION::CALL);
 
-	unsigned char* shellcode = CreateShellcode(funcHeaders, relativeAddrBytes, sizeofShellcode, sizeofFuncHeaders, nbArgsPushed, true);
+	unsigned char* shellcode = CreateShellcode(funcHeaders, finalAddrBytes, sizeofShellcode, sizeofFuncHeaders, nbArgsPushed, true);
 
 	WriteShellcodeInMemory(hProcess, pRemoteBuffer, shellcode, sizeofShellcode);
 
 	HANDLE hThread = CreateRemThread(hProcess, pRemoteBuffer);
 
-	CloseProperly(hProcess, hThread, pRemoteBuffer, shellcode);
+	CloseProperly(hProcess, hThread, pRemoteBuffer, finalAddrBytes, shellcode);
 }
 
 template<typename... Args>
-void MultipleCallGameFunc(HANDLE hProcess, DWORD* callAddressesArray, const int sizeofArray, int* sizeofArrays, int* nbArgsPushedArray, unsigned char* headersArray, Args... shellcodeHeaders)
+void MultipleCallGameFunc(HANDLE hProcess, DWORD* callAddressesArray, const int sizeofArray, int* sizeofArrays, int* nbArgsPushedArray, Args... shellcodeHeaders)
 {
 	int sizeofWholeShellcode = 0;
 
 	//Start at one to not take sizeofCalls
 	for (int i = 1; i < sizeofArray; i++)
 	{
-		sizeofWholeShellcode += (sizeofArrays[i] + SIZEOF_RELATIVE_ADDR_CALL + SIZEOF_END_SHELLCODE);
+		sizeofWholeShellcode += (sizeofArrays[i] + SIZEOF_RELATIVE_ADDR_CALL_WITH_INSTRUCTION + SIZEOF_END_SHELLCODE);
 	}
 
 	LPVOID pRemoteBuffer = VirtualAllocation(hProcess, sizeofWholeShellcode);
@@ -196,7 +218,7 @@ void MultipleCallGameFunc(HANDLE hProcess, DWORD* callAddressesArray, const int 
 	int sizeofCurrentAndPrecedentFunc = 0;
 
 	for (unsigned char* shellcodeHeadersArray : { shellcodeHeaders... }) {
-		int sizeofCurrentShellcode = sizeofArrays[i + 1] + SIZEOF_RELATIVE_ADDR_CALL + SIZEOF_END_SHELLCODE;
+		int sizeofCurrentShellcode = sizeofArrays[i + 1] + SIZEOF_RELATIVE_ADDR_CALL_WITH_INSTRUCTION + SIZEOF_END_SHELLCODE;
 		int sizeofCurrentFuncHeader = sizeofArrays[i + 1];
 
 		if (i == 0)
@@ -209,7 +231,7 @@ void MultipleCallGameFunc(HANDLE hProcess, DWORD* callAddressesArray, const int 
 
 			for (int x = 1; x < (i + 1); x++)
 			{
-				int sizeofPrecedentShellcode = sizeofArrays[x] + SIZEOF_RELATIVE_ADDR_CALL + SIZEOF_END_SHELLCODE;
+				int sizeofPrecedentShellcode = sizeofArrays[x] + SIZEOF_RELATIVE_ADDR_CALL_WITH_INSTRUCTION + SIZEOF_END_SHELLCODE;
 
 				if ((x + 1) < i)
 				{
@@ -229,84 +251,112 @@ void MultipleCallGameFunc(HANDLE hProcess, DWORD* callAddressesArray, const int 
 
 		if (i > 0)
 		{
-			int sizeofPrecedentShellcode = sizeofArrays[i] + SIZEOF_RELATIVE_ADDR_CALL + SIZEOF_END_SHELLCODE;
+			int sizeofPrecedentShellcode = sizeofArrays[i] + SIZEOF_RELATIVE_ADDR_CALL_WITH_INSTRUCTION + SIZEOF_END_SHELLCODE;
 			pRemoteBufferOffset += sizeofPrecedentShellcode;
 		}
 
-		unsigned char* relativeAddrBytes = GetRelativeAddr(pRemoteBuffer, callAddressesArray[i], sizeofCurrentAndPrecedentFunc);
+		DWORD relativeAddrBytes = GetRelativeAddr(pRemoteBuffer, callAddressesArray[i], sizeofCurrentAndPrecedentFunc);
+		unsigned char* finalAddrBytes = GetFinalAddrWithInstruction(relativeAddrBytes, INSTRUCTION::CALL);
 
-		shellcode = CreateShellcode(shellcodeHeadersArray, relativeAddrBytes, sizeofCurrentShellcode, sizeofCurrentFuncHeader, nbArgsPushedArray[i], isRet);
+		shellcode = CreateShellcode(shellcodeHeadersArray, finalAddrBytes, sizeofCurrentShellcode, sizeofCurrentFuncHeader, nbArgsPushedArray[i], isRet);
 
 		WriteShellcodeInMemory(hProcess, pRemoteBufferOffset, shellcode, sizeofCurrentShellcode);
+
+		delete[] finalAddrBytes;
+		delete[] shellcode;
 
 		i++;
 	}
 
 	HANDLE hThread = CreateRemThread(hProcess, pRemoteBuffer);
 
-	CloseProperly(hProcess, hThread, pRemoteBuffer, shellcode);
+	CloseProperly(hProcess, hThread, pRemoteBuffer);
 }
+#pragma endregion
 
-void CallPrintConsoleFunc(HANDLE hProcess)
+#pragma region Game Funcs To Call
+/// <summary>
+/// Call the console func to write message in the game
+/// Multiple call with malloc + console func
+/// </summary>
+/// <param name="hProcess"></param>
+void MultipleCallPrintConsoleFunc(HANDLE hProcess)
 {
+	// Header for malloc func (param)
 	unsigned char mallocHeader[] = {
 		0x6A, 0x0A
 	};
 
+	// Header for printConsoleHeader func param and stuff
 	unsigned char printConsoleHeader[] = {
 		0x89, 0xC3, 0xC6, 0x03, 0x49, 0xC6, 0x43, 0x01, 0x4E, 0xC6, 0x43, 0x02, 0x4A, 0xC6, 0x43, 0x03, 0x45, 0xC6, 0x43, 0x04, 0x43, 0xC6, 0x43, 0x05, 0x54, 0xC6, 0x43, 0x06, 0x49, 0xC6, 0x43, 0x07, 0x4F, 0xC6, 0x43, 0x08, 0x4E, 0xC6, 0x43, 0x09, 0x21, 0x53
 	};
 
-	unsigned char headersArray[] = {
-		*mallocHeader,
-		*printConsoleHeader
-	};
-
+	// Number of args pushed to the stack by calling array
 	int nbArgsPushedArray[] = {
 		1,
 		1
 	};
 
-	//int* mallocAddr = (int*)malloc(sizeof(int));
-	//DWORD mallocAddrFunc = (DWORD)mallocAddr;
-
+	// This addr changes so you have to find it to use correctly this method
+	// This was an example so you can make whatever you want 
 	DWORD mallocAddrFunc = 0x769C74F0;
-	DWORD printConsoleFunc = 0x004DAD50;
+	// This is the game function I want to call
+	DWORD printConsoleAddrFunc = 0x004DAD50;
 
+	// Addresses array of all my addr I want to call
 	DWORD callAddressesArray[] = {
 		mallocAddrFunc,
-		printConsoleFunc
+		printConsoleAddrFunc
 	};
 
+	// Sizeof call addr array with my headers
 	int sizeofArrays[] = {
 		sizeof callAddressesArray / sizeof callAddressesArray[0],
 		sizeof mallocHeader,
 		sizeof printConsoleHeader
 	};
 
+	// Sizeof my size array to get the number of elements in it
 	int sizeofArray = sizeof sizeofArrays / sizeof sizeofArrays[0];
 
-	MultipleCallGameFunc(hProcess, callAddressesArray, sizeofArray, sizeofArrays, nbArgsPushedArray, headersArray, mallocHeader, printConsoleHeader);
+	// Fill the func with everything to call multiple func in my game
+	MultipleCallGameFunc(hProcess, callAddressesArray, sizeofArray, sizeofArrays, nbArgsPushedArray, mallocHeader, printConsoleHeader);
 }
 
-void CallSinglePrintConsole(HANDLE hProcess)
+/// <summary>
+/// Also calling the console func to write message in the game
+/// Instead of malloc I allocated memory outside my shellcode
+/// Which is cleaner and easier to do
+/// </summary>
+/// <param name="hProcess"></param>
+void SingleCallPrintConsoleFunc(HANDLE hProcess)
 {
+	// Allocate new memory space to store a message in it
+	const char* message = "PRINT CONSOLE FUNC CALLED WITHOUT DLL !";
+	LPVOID pMessageBuffer = VirtualAllocation(hProcess, strlen(message));
+	WriteProcessMemory(hProcess, pMessageBuffer, message, strlen(message), NULL);
+
+	// Addr of my game func to call
 	DWORD printConsoleFunc = 0x004DAD50;
+	// Push addr of my message into the stack
+	unsigned char* finalAddrPushArgPrintConsole = GetFinalAddrWithInstruction((DWORD)pMessageBuffer, INSTRUCTION::PUSH, false);
+	// Sending my push instruction with addr of my message with the addr of printConsoleFunc to call it after the push bytes code
+	CallGameFunc(hProcess, finalAddrPushArgPrintConsole, printConsoleFunc, SIZEOF_RELATIVE_ADDR_CALL_WITH_INSTRUCTION);
 
-	unsigned char printConsoleHeader[] = {
-		0x68, 0xE4, 0xA8, 0x55, 0x00
-	};
-
-	CallGameFunc(hProcess, printConsoleHeader, printConsoleFunc, sizeof printConsoleHeader);
+	// Release memory to avoid leaks
+	VirtualFreeEx(hProcess, pMessageBuffer, 0, MEM_RELEASE);
+	delete[] finalAddrPushArgPrintConsole;
 }
+#pragma endregion
 
 int main()
 {
 	const char* processName = "ac_client.exe";
 	HANDLE hProcess = OpenProc(processName);
 
-	CallPrintConsoleFunc(hProcess);
-	//CallSinglePrintConsole(hProcess);
+	//CallPrintConsoleFunc(hProcess);
+	SingleCallPrintConsoleFunc(hProcess);
 
 	std::cout << "Injection ended successfully.\n\n";
 
